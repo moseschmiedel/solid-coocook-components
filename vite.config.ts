119d2f8f716183251d2daee18d7078e345881fdb
Mose Schmiedel <mose@schmiednet.de>
2022-05-25 15:33:09 +0200

    Add Config for staging build step

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,45 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  build: {
-      target: 'esnext',
-      polyfillDynamicImport: false,
-      rollupOptions: {
-          output: {
-              entryFileNames: "assets/[name].js",
-              chunkFileNames: "assets/[name].js",
-              assetFileNames: "assets/[name][extname]",
-          }
-      }
-  },
+const commonConfig = defineConfig({
+    plugins: [solidPlugin()],
+});
+
+export default defineConfig(({ command, mode }) => {
+    if (command !== 'build')
+        return {
+            ...commonConfig,
+        };
+    if (mode !== 'staging')
+        return {
+            ...commonConfig,
+            build: {
+                target: 'esnext',
+                polyfillDynamicImport: false,
+                rollupOptions: {
+                    output: {
+                        entryFileNames: "assets/[name].js",
+                        chunkFileNames: "assets/[name].js",
+                        assetFileNames: "assets/[name][extname]",
+                    }
+                }
+            },
+        };
+
+    return {
+        ...commonConfig,
+        build: {
+            target: 'esnext',
+            polyfillDynamicImport: false,
+            minify: false,
+            rollupOptions: {
+                output: {
+                    entryFileNames: "assets/[name].js",
+                    chunkFileNames: "assets/[name].js",
+                    assetFileNames: "assets/[name][extname]",
+                }
+            }
+        },
+    };
 
 });
