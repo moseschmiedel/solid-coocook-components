98328e7c18a112c1cecdb87d668eaf830a473c7a
Mose Schmiedel <mose@schmiednet.de>
2022-05-31 19:00:14 +0200

    Use css-modules

--- src/util/css.ts
+++ src/util/css.ts
@@ -1,5 +1,5 @@
 export function c(...classes: Array<string>): string {
-    console.log('classes');
-    console.log(classes);
+    // console.log('classes');
+    // console.log(classes);
     return classes.join(' ');
 }
